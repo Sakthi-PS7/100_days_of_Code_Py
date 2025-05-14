705f38a8e8afe7fca1e74cdd27bb5dc8eaf66600
Sakthivel Ponnampalayam Sivakumar <ponnampalayamsivak.s@northeastern.edu>
2025-05-14 00:39:29 -0400

    Day_3 of 100 days of Code

---
+++ Day_3.py
@@ -0,0 +1,18 @@
+#If else
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height>=120:
+    print('Hurray!, you are eligible for the ride')
+else:
+    print("Sorry!,you are not tall enough :(")
+
+#Modulo
+Given_number=int(input("Enter any random number: "))
+if Given_number % 2 == 0:
+    print("The given number is even")
+else:
+    print("The number is odd")
+
+#Nesting and elif
+mani = 20
