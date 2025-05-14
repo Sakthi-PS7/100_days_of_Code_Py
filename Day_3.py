5313ba2db361c9f4ad4f493e6f36c33cf8aebe0b
Sakthivel Ponnampalayam Sivakumar <ponnampalayamsivak.s@northeastern.edu>
2025-05-14 00:40:16 -0400

    day3 of 100days of code

--- Day_3.py
+++ Day_3.py
@@ -15,4 +15,35 @@ else:
     print("The number is odd")
 
 #Nesting and elif
-mani = 20
+math_score = int(input("What is your math score:"))
+eng_score = int(input("What is your eng score:"))
+
+if math_score >= 90:
+    if eng_score >= 90:
+        print("you are good in both subjects")
+    else:
+        print("you are only good at math")
+else:
+    if eng_score >= 90:
+        print("you are only good at english")
+    else:
+        print("you are poor in both subjects")
+
+#Multiple if statements
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
+#python pizza
+
