28e962e6d7f009c4cb7efaa0370f4973191e6570
Sakthivel Ponnampalayam Sivakumar <ponnampalayamsivak.s@northeastern.edu>
2025-05-14 23:48:44 -0400

    Day_3 of 100 Days of Code

--- Day_3.py
+++ Day_3.py
@@ -48,4 +48,86 @@ else:
     print("Sorry you have to grow taller before you can ride.")
 
 #python pizza
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+Bill = 0
+
+#small pizza
+if size == "S":
+    Bill+=15
+#medium pizza
+elif size == "M":
+    Bill+=20
+#large pizza
+elif size == "L":
+    Bill+=25
+else:
+    print("you entered the wrong value :(")
+
+#pepperoni
+if pepperoni == "Y":
+    if size =="S":
+        Bill+=2
+    else:
+        Bill+=3
+
+#exta cheese
+if extra_cheese == "Y":
+    Bill+=1
+
+print(f"Your final bill is: ${Bill}.")
+
+#Logical operators
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif 45<=age<=55:
+        bill = 0
+        print("Free ride for aged people")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+#Treasure Island
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+Entry1 = input("Choose either 'L' or 'R'")
+if Entry1 == "L":
+    Entry2 = input("Do you wanna 'Swim' or 'wait'")
+    if Entry2 == "wait":
+        Entry3 = input("Which door, 'red' or 'blue' or 'yellow'")
+        if Entry3 == "yellow":
+            print('Congrats! you win! :)))')
+        elif Entry3 == 'Red':
+            print('Burned by fire, game over')
+        elif Entry3 == 'Blue':
+            print('Eaten by beasts, game over')
+        else:
+            print('game over')
+    else:
+        print('Attacked by trout, game over')
+else:
+    print('Fall into hole, game over')
 
