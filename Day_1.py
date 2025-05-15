026befc286f6ef1a50663aab07e098c650200ceb
Sakthivel Ponnampalayam Sivakumar <ponnampalayamsivak.s@northeastern.edu>
2025-05-14 23:35:28 -0400

    Day_1 of 100 Days of Code

---
+++ Day_1.py
@@ -0,0 +1,31 @@
+#Day1
+
+
+#Printing
+print('hello mama')
+
+
+#String Manipulation
+print("Hello world!\nHello world!\nHello world!")
+print("Hello"+" mame")
+
+#Inputs
+print("My name is " + input("Type your name here:" )+'!')
+
+#Variables
+username=input('Enter a string:')
+length=len(username)
+print(username,length)
+
+#Variable Naming
+name = "sakthi"
+length = len(name)
+print(name)
+print(length)
+
+#band Name Generator Program
+city_name = input('Enter your birth city?\n')
+pet_name = input('Enter your pet name?\n')
+#creating a variable for storing the band_name
+band_name = city_name+' '+pet_name
+print('The name of your band is:',band_name)
